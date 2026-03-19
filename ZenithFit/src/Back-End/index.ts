bb49d90a7106a273e8db282db624ae2c3cb08d94
Gabriel Meireles <gabrielmeireles0007@gmail.com>
2026-03-19 00:35:37 -0300

    adicionado back end, e crud de cliente com colunas básicas

--- ZenithFit/src/Back-End/index.ts
+++ ZenithFit/src/Back-End/index.ts
@@ -1,9 +1,35 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
+import {connection} from "./src/db.js";
+import { prisma } from "./src/db.js";
+//import cors from "cors";
 
 const app = express();
+app.use(express.json());
+//app.use(cors());
+connection();
 
-app.get("/teste", (req, res) => {
-    res.json("Você acessou a rota inicial");
+app.post("/register", async (req: Request, res: Response) => {
+  
+  try {
+
+    const {name, email, password, cep} = req.body;
+
+    if(!name || !email || !password || !cep) {
+        res
+          .status(400)
+          .json({ message: "Todos os campos são obrigatórios." });
+        return;
+    }
+
+    const newUser = await prisma.cliente.create({
+      data: {name: name, email: email, password: password, cep: cep},
+    });
+
+    res.json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: "Erro no servidor" });
+    return;
+  }
 });
 
 app.listen(3000, () => {
